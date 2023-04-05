1586ddbe2d454092847b5255a8b8702fd7ba6199
Kovak <kovac1066@gmail.com>
2023-04-04 17:26:24 -0700

    comment out this mixin until we can figure out why it crash now

--- MKCore/src/main/java/com/chaosbuffalo/mkcore/mixins/ItemStackClientMixins.java
+++ MKCore/src/main/java/com/chaosbuffalo/mkcore/mixins/ItemStackClientMixins.java
@@ -20,54 +20,55 @@ import java.util.List;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackClientMixins {
+//FIXME: This is broken, causes a crash in text visiting
 
-    @Unique
-    private List<Component> tooltipList;
-
-    @Unique
-    private Player player;
-
-    @Shadow
-    public abstract Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot equipmentSlot);
-
-    // lets us remove attributes only from the automatic tooltip generation in item stack
-    @Redirect(
-            method = "getTooltipLines(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/TooltipFlag;)Ljava/util/List;",
-            at = @At(
-                    target = "Lnet/minecraft/world/item/ItemStack;getAttributeModifiers(Lnet/minecraft/world/entity/EquipmentSlot;)Lcom/google/common/collect/Multimap;",
-                    value = "INVOKE"
-            )
-    )
-    private Multimap<Attribute, AttributeModifier> proxyGetAttributeModifiers(ItemStack itemStack, EquipmentSlot equipmentSlot) {
-        // Don't follow our path if it's building the search tree during startup
-        if (player == null) {
-            return getAttributeModifiers(equipmentSlot);
-        }
-
-        if (tooltipList != null) {
-            AttributeTooltipManager.renderTooltip(tooltipList, player, itemStack, equipmentSlot);
-        }
-        return ImmutableMultimap.of();
-    }
-
-    @ModifyVariable(
-            method = "getTooltipLines(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/TooltipFlag;)Ljava/util/List;",
-            at = @At("HEAD"),
-            argsOnly = true
-    )
-    private Player capturePlayer(Player player) {
-        this.player = player;
-        return player;
-    }
-
-    @ModifyVariable(
-            method = "getTooltipLines(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/TooltipFlag;)Ljava/util/List;",
-            at = @At("STORE"),
-            index = 3,
-            ordinal = 0
-    )
-    private List<Component> captureList(List<Component> list) {
-        this.tooltipList = list;
-        return list;
-    }
+//    @Unique
+//    private List<Component> tooltipList;
+//
+//    @Unique
+//    private Player player;
+//
+//    @Shadow
+//    public abstract Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot equipmentSlot);
+//
+//    // lets us remove attributes only from the automatic tooltip generation in item stack
+//    @Redirect(
+//            method = "getTooltipLines(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/TooltipFlag;)Ljava/util/List;",
+//            at = @At(
+//                    target = "Lnet/minecraft/world/item/ItemStack;getAttributeModifiers(Lnet/minecraft/world/entity/EquipmentSlot;)Lcom/google/common/collect/Multimap;",
+//                    value = "INVOKE"
+//            )
+//    )
+//    private Multimap<Attribute, AttributeModifier> proxyGetAttributeModifiers(ItemStack itemStack, EquipmentSlot equipmentSlot) {
+//        // Don't follow our path if it's building the search tree during startup
+//        if (player == null) {
+//            return getAttributeModifiers(equipmentSlot);
+//        }
+//
+//        if (tooltipList != null) {
+//            AttributeTooltipManager.renderTooltip(tooltipList, player, itemStack, equipmentSlot);
+//        }
+//        return ImmutableMultimap.of();
+//    }
+//
+//    @ModifyVariable(
+//            method = "getTooltipLines(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/TooltipFlag;)Ljava/util/List;",
+//            at = @At("HEAD"),
+//            argsOnly = true
+//    )
+//    private Player capturePlayer(Player player) {
+//        this.player = player;
+//        return player;
+//    }
+//
+//    @ModifyVariable(
+//            method = "getTooltipLines(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/TooltipFlag;)Ljava/util/List;",
+//            at = @At("STORE"),
+//            index = 3,
+//            ordinal = 0
+//    )
+//    private List<Component> captureList(List<Component> list) {
+//        this.tooltipList = list;
+//        return list;
+//    }
 }
