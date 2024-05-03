3463da13df7e1b6780af8acb49ac3f971b627a3a
ralekdev <ralekdev@gmail.com>
2024-05-02 22:57:06 -0700

    Convert StructureEventRequirement to Codec

--- MKNpc/src/main/java/com/chaosbuffalo/mknpc/world/gen/feature/structure/events/requirements/StructureHasNotableRequirement.java
+++ MKNpc/src/main/java/com/chaosbuffalo/mknpc/world/gen/feature/structure/events/requirements/StructureHasNotableRequirement.java
@@ -1,33 +1,32 @@
 package com.chaosbuffalo.mknpc.world.gen.feature.structure.events.requirements;
 
-import com.chaosbuffalo.mkcore.serialization.attributes.ResourceLocationAttribute;
 import com.chaosbuffalo.mknpc.MKNpc;
 import com.chaosbuffalo.mknpc.capabilities.WorldStructureManager;
 import com.chaosbuffalo.mknpc.npc.MKStructureEntry;
-import com.chaosbuffalo.mknpc.npc.NpcDefinitionManager;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 
 public class StructureHasNotableRequirement extends StructureEventRequirement {
     public final static ResourceLocation TYPE_NAME = new ResourceLocation(MKNpc.MODID,
             "struct_requirement.has_notable");
-    protected ResourceLocationAttribute npcDefinition = new ResourceLocationAttribute(
-            "npcDefinition", NpcDefinitionManager.INVALID_NPC_DEF);
+    public static final Codec<StructureHasNotableRequirement> CODEC = RecordCodecBuilder.<StructureHasNotableRequirement>mapCodec(builder -> {
+        return builder.group(
+                ResourceLocation.CODEC.fieldOf("npcDefinition").forGetter(i -> i.npcDefinition)
+        ).apply(builder, StructureHasNotableRequirement::new);
+    }).codec();
+
+    protected final ResourceLocation npcDefinition;
 
     public StructureHasNotableRequirement(ResourceLocation defName) {
-        this();
-        npcDefinition.setValue(defName);
-
-    }
-
-    public StructureHasNotableRequirement() {
         super(TYPE_NAME);
-        addAttribute(npcDefinition);
+        this.npcDefinition = defName;
     }
 
     @Override
     public boolean meetsRequirements(MKStructureEntry entry,
                                      WorldStructureManager.ActiveStructure activeStructure, Level world) {
-        return entry.hasNotableOfType(npcDefinition.getValue());
+        return entry.hasNotableOfType(npcDefinition);
     }
 }
