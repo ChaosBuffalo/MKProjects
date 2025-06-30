40b199cafc640cec2fc0d7f859929dc916046067
ralekdev <ralekdev@gmail.com>
2025-06-29 23:06:51 -0700

    Update NeoForge & MDG and remove old gradle plugin

--- MKNpc/src/main/java/com/chaosbuffalo/mknpc/components/NpcComponents.java
+++ MKNpc/src/main/java/com/chaosbuffalo/mknpc/components/NpcComponents.java
@@ -2,12 +2,13 @@ package com.chaosbuffalo.mknpc.components;
 
 import com.chaosbuffalo.mknpc.MKNpc;
 import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class NpcComponents {
-    public static final DeferredRegister.DataComponents COMPONENT_TYPES = DeferredRegister.createDataComponents(MKNpc.MODID);
+    public static final DeferredRegister.DataComponents COMPONENT_TYPES = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, MKNpc.MODID);
 
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<SpawnerDataComponent>> SPAWNER_DATA = COMPONENT_TYPES
             .registerComponentType("spawner_data", builder -> {
--- MKWeapons/src/main/java/com/chaosbuffalo/mkweapons/components/WeaponsComponents.java
+++ MKWeapons/src/main/java/com/chaosbuffalo/mkweapons/components/WeaponsComponents.java
@@ -2,12 +2,13 @@ package com.chaosbuffalo.mkweapons.components;
 
 import com.chaosbuffalo.mkweapons.MKWeapons;
 import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class WeaponsComponents {
-    public static final DeferredRegister.DataComponents COMPONENT_TYPES = DeferredRegister.createDataComponents(MKWeapons.MODID);
+    public static final DeferredRegister.DataComponents COMPONENT_TYPES = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, MKWeapons.MODID);
 
 
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<MeleeEffectsComponent>> MELEE_EFFECTS = COMPONENT_TYPES
